abbdc9905cea89fb328ac91e3b437a35bb4551d5
Thunder Ralston <thunderralston@gmail.com>
2026-06-19 14:46:51 -0400

    Added First Script for wheel

---
+++ scenes/node_2d_wheel.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	
+	
+	if event.is_action_pressed(&"left_click"):
+		
+		print(randi() % 5 + 1) # Print a number between 1 and 5
+
+		
