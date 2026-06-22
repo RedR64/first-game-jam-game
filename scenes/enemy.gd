0193b31f064bf2d72e927152b47660db6bed7bf2
RedR64 <70602858+RedR64@users.noreply.github.com>
2026-06-22 18:10:51 -0400

    Combat Foundation
    added the files for the combat system. though no combat system atm

---
+++ scenes/enemy.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+var spinVars = Spin.new;
+var health = spinVars.health;
+var defense = spinVars.defense;
+var damage = spinVars.damage;
+var agility = spinVars.agility;
+var savingThrow = spinVars.savingThrow;
+var currencyMult = spinVars.currency_multiplier;
