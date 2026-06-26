94fa92111afe8fef9980739af7ab8ea347acfd47
RedR64 <70602858+RedR64@users.noreply.github.com>
2026-06-25 22:42:15 -0400

    Shadowbox
    Stupid bug

--- scenes/office_background.gd
+++ scenes/office_background.gd
@@ -1,18 +1,7 @@
 extends Node2D
-var open_closed = 0
 var shop_scene = preload("res://scenes/shop_screen.tscn").instantiate()
-func _on_shop_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_shop_test_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("left_click"):
-		if open_closed == 1:
-			shop_exit()
-			
-		else:
 			shop()
-		
 func shop():
-	open_closed = 1
 	get_tree().root.add_child(shop_scene)
-	
-func shop_exit():
-	open_closed = 0
-	get_tree().root.remove_child(shop_scene)
--- scenes/shop_screen.gd
+++ scenes/shop_screen.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-
-
 func _on_shop_exit_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("left_click"):
 		shop_exit()
-		
+
 func shop_exit():
-	var current_scene = self
-	#get_tree().root.remove_child(current_scene)
-	var shopScreen = get_tree().root.get_child(current_scene)
-	get_tree().root.remove_child(shopScreen)
+	get_tree().root.remove_child(self)
